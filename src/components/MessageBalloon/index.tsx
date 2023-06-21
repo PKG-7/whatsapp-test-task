e4785b546eaded6133f1d7f2dee4ab6507f9f844
PKG-7 <pavel.k.group@gmail.com>
2023-06-21 22:10:25 +0300

    chat balloon time position fixed

--- src/components/MessageBalloon/index.tsx
+++ src/components/MessageBalloon/index.tsx
@@ -18,12 +18,15 @@ export default function MessageBalloon({
             <div
                 className={`flex  min-w-[5%] gap-2 max-w-[65%] h-max ${backgroundColor} p-2 text-white rounded-lg ${borderRounded} mb-3`}
             >
-                <div className='flex flex-col w-full break-words'>
-                    <span>{message.text}</span>
+                <div className='flex flex-col w-full relative break-words'>
+                    <div className='pr-8 relative'>
+                        <span>{message.text}</span>
+                        <span className='absolute bottom-[-6px] right-[-2px] text-[hsla(0,0%,100%,0.6)] text-xs mt-1'>
+                            {message.time}
+                        </span>
+                    </div>
                 </div>
-                <div className='flex items-end h-full text-[hsla(0,0%,100%,0.6)] text-xs mt-1'>
-                    <span>{message.time}</span>
-                </div>
+                {/* <div className='flex items-end h-full text-[hsla(0,0%,100%,0.6)] text-xs mt-1'></div> */}
             </div>
         </div>
     )
